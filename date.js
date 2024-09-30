3eb48c437495f5f60e9735627fb9ed5022e8a13e
VadimKorobec <_s_e_t__@ukr.net>
2024-09-30 15:41:59 +0300

    added some code

--- date.js
+++ date.js
@@ -4,4 +4,31 @@ const dateOptionsSelectElement = document.getElementById("date-options");
 const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
-console.log(month);
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+const formattedDate = `${day}-${month}-${year}`;
+
+currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener("change", () => {});
+
+const exampleSentence = "selur pmaCedoCeerf".split(" ").reverse().join("");
+console.log(exampleSentence);
+
+switch (dateOptionsSelectElement.value) {
+  case "yyyy-mm-dd":
+    currentDateParagraph.textContent = formattedDate
+      .split("-")
+      .reverse()
+      .join("-");
+    break;
+  case "mm-dd-yyyy-h-mm":
+    currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+    break;
+  default:
+    currentDateParagraph.textContent = formattedDate;
+}
+
+console.log(formattedDate);
