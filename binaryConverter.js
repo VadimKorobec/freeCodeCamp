c521ca7a47066b6bc5dd7e6aac254b00d53a42de
VadimKorobec <_s_e_t__@ukr.net>
2024-10-02 17:18:21 +0300

    added some code

--- binaryConverter.js
+++ binaryConverter.js
@@ -1,3 +1,7 @@
+const a = () => {
+  return "freeCodeCamp " + b();
+};
+
 //input
 const numberInput = document.getElementById("number-input");
 
@@ -8,14 +12,42 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+  //   const inputs = [];
+  //   const quotients = [];
+  //   const remainders = [];
+
+  //   if (input === 0) {
+  //     result.innerText = "0";
+  //     return;
+  //   }
+
+  //   while (input > 0) {
+  //     const quotient = Math.floor(input / 2);
+  //     const remainder = input % 2;
+  //     inputs.push(input);
+  //     quotients.push(quotient);
+  //     remainders.push(remainder);
+  //     input = quotient;
+  //   }
+
+  //   console.log("Inputs: ", inputs);
+  //   console.log("Quotients: ", quotients);
+  //   console.log("Remainders: ", remainders);
+
+  //   result.innerText = remainders.reverse().join("");
+
+  let binary = "";
+
+  if (input === 0) {
+    binary = "0";
+  }
 
   while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    input = quotient;
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
   }
+
+  result.innerText = binary;
 };
 
 const checkUserInput = () => {
