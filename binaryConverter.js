929d57217655b67e22de88fab0f695d8d2d1b941
VadimKorobec <_s_e_t__@ukr.net>
2024-10-03 16:58:04 +0300

    added some code

--- binaryConverter.js
+++ binaryConverter.js
@@ -1,7 +1,3 @@
-const a = () => {
-  return "freeCodeCamp " + b();
-};
-
 //input
 const numberInput = document.getElementById("number-input");
 
@@ -12,43 +8,14 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  //   const inputs = [];
-  //   const quotients = [];
-  //   const remainders = [];
-
-  //   if (input === 0) {
-  //     result.innerText = "0";
-  //     return;
-  //   }
-
-  //   while (input > 0) {
-  //     const quotient = Math.floor(input / 2);
-  //     const remainder = input % 2;
-  //     inputs.push(input);
-  //     quotients.push(quotient);
-  //     remainders.push(remainder);
-  //     input = quotient;
-  //   }
-
-  //   console.log("Inputs: ", inputs);
-  //   console.log("Quotients: ", quotients);
-  //   console.log("Remainders: ", remainders);
-
-  //   result.innerText = remainders.reverse().join("");
-
-  let binary = "";
-
-  if (input === 0) {
-    binary = "0";
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
+
+const showAnimation = () => {};
 
 const checkUserInput = () => {
   if (
@@ -61,7 +28,11 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  if (parseInt(numberInput.value) === 5) {
+    showAnimation();
+    return;
+  }
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   reset();
 };
 
